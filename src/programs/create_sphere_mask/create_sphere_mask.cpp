dc39840e6a006dfd72929a9efe3b92a6b068e32a
Heidy Elkhaligy <heidy.elkhaligy@wisc.edu>
2025-08-26 13:36:38 -0400

    adding create black sphere mask option

--- src/programs/create_sphere_mask/create_sphere_mask.cpp
+++ src/programs/create_sphere_mask/create_sphere_mask.cpp
@@ -11,7 +11,7 @@ class
 };
 
 void create_white_sphere_mask(Image* mask_file, int x_sphere_center, int y_sphere_center, int z_spehere_center, float radius);
-
+void create_black_sphere_mask(Image* mask_file, int x_sphere_center, int y_sphere_center, int z_spehere_center, float radius);
 IMPLEMENT_APP(create_sphere_mask)
 
 // override the DoInteractiveUserInput
@@ -21,10 +21,11 @@ void create_sphere_mask::DoInteractiveUserInput( ) {
     wxString output_filename;
     int      box_size;
     float    pixel_size;
-    int      x_mask_center      = 1;
-    int      y_mask_center      = 1;
-    int      z_mask_center      = 1;
-    int      sphere_mask_radius = 1;
+    int      x_mask_center       = 1;
+    int      y_mask_center       = 1;
+    int      z_mask_center       = 1;
+    int      sphere_mask_radius  = 1;
+    bool     create_white_sphere = true;
 
     UserInput* my_input = new UserInput("create_sphere_mask", 1.0);
     output_filename     = my_input->GetFilenameFromUser("Output mask file name", "Name of the output mask file", "my_mask.mrc", false);
@@ -34,31 +35,33 @@ void create_sphere_mask::DoInteractiveUserInput( ) {
     y_mask_center       = my_input->GetIntFromUser("Y center pf the mask (pixels)", "The Y center of the provided mask or the created mask by the program, 1 = default is 0.5 * box size", "1", 1);
     z_mask_center       = my_input->GetIntFromUser("Z center of the mask (pixels)", "The Z center of the provided mask or the created mask by the program, 1 = default is 0.5 * box size", "1", 1);
     sphere_mask_radius  = my_input->GetIntFromUser("Sphere mask radius (pixels)", "The radius of the provided mask or the created mask by the program, 1 = default is 0.3 * box size", "1", 1);
+    create_white_sphere = my_input->GetYesNoFromUser("Create a white sphere mask?", "If, Yes a white sphere mask inside the box will be create. Otherwise, a black sphere mask on white background will be created", "Yes");
 
     delete my_input;
 
-    my_current_job.Reset(9); // + 1 the number shown in the string below
-    my_current_job.ManualSetArguments("tifiiii", output_filename.ToUTF8( ).data( ),
+    my_current_job.Reset(10); // + 1 the number shown in the string below
+    my_current_job.ManualSetArguments("tifiiiib", output_filename.ToUTF8( ).data( ),
                                       box_size,
                                       pixel_size,
                                       x_mask_center,
                                       y_mask_center,
                                       z_mask_center,
-                                      sphere_mask_radius);
+                                      sphere_mask_radius,
+                                      create_white_sphere);
 }
 
 // override the do calculation method which will be what is actually run..
 
 bool create_sphere_mask::DoCalculation( ) {
     // get the arguments for this job..
-    wxString output_filename    = my_current_job.arguments[0].ReturnStringArgument( );
-    int      box_size           = my_current_job.arguments[1].ReturnIntegerArgument( );
-    float    pixel_size         = my_current_job.arguments[2].ReturnFloatArgument( );
-    int      x_mask_center      = my_current_job.arguments[3].ReturnIntegerArgument( );
-    int      y_mask_center      = my_current_job.arguments[4].ReturnIntegerArgument( );
-    int      z_mask_center      = my_current_job.arguments[5].ReturnIntegerArgument( );
-    int      sphere_mask_radius = my_current_job.arguments[6].ReturnIntegerArgument( );
-
+    wxString output_filename     = my_current_job.arguments[0].ReturnStringArgument( );
+    int      box_size            = my_current_job.arguments[1].ReturnIntegerArgument( );
+    float    pixel_size          = my_current_job.arguments[2].ReturnFloatArgument( );
+    int      x_mask_center       = my_current_job.arguments[3].ReturnIntegerArgument( );
+    int      y_mask_center       = my_current_job.arguments[4].ReturnIntegerArgument( );
+    int      z_mask_center       = my_current_job.arguments[5].ReturnIntegerArgument( );
+    int      sphere_mask_radius  = my_current_job.arguments[6].ReturnIntegerArgument( );
+    bool     create_white_sphere = my_current_job.arguments[7].ReturnBoolArgument( );
     // initiate I/O variables
     MRCFile my_output_filename(output_filename.ToStdString( ), true);
 
@@ -81,7 +84,12 @@ bool create_sphere_mask::DoCalculation( ) {
     Image my_white_mask_smooth;
     my_white_mask_smooth.Allocate(box_size, box_size, box_size, true);
     my_white_mask_smooth.SetToConstant(0.0);
-    create_white_sphere_mask(&my_white_mask_smooth, x_mask_center, y_mask_center, z_mask_center, sphere_mask_radius);
+    if ( create_white_sphere ) {
+        create_white_sphere_mask(&my_white_mask_smooth, x_mask_center, y_mask_center, z_mask_center, sphere_mask_radius);
+    }
+    else {
+        create_black_sphere_mask(&my_white_mask_smooth, x_mask_center, y_mask_center, z_mask_center, sphere_mask_radius);
+    }
     my_white_mask_smooth.ForwardFFT( );
     my_white_mask_smooth.GaussianLowPassFilter((pixel_size * 2) / 150);
     my_white_mask_smooth.BackwardFFT( );
@@ -121,4 +129,35 @@ void create_white_sphere_mask(Image* mask_file, int x_sphere_center, int y_spher
         }
     }
     //mask_file->QuickAndDirtyWriteSlices("make_white_sphere_mask_inside_function.mrc", 1, mask_file->logical_z_dimension);
+}
+
+void create_black_sphere_mask(Image* mask_file, int x_sphere_center, int y_sphere_center, int z_spehere_center, float radius) {
+
+    int   boxsize = mask_file->logical_x_dimension;
+    int   i, j, k;
+    int   dx, dy, dz;
+    float d;
+    // initialize the mask file to be 1.0
+    mask_file->SetToConstant(1.0);
+
+    long pixel_counter = 0;
+
+    for ( k = 0; k < mask_file->logical_z_dimension; k++ ) {
+        for ( j = 0; j < mask_file->logical_y_dimension; j++ ) {
+            for ( i = 0; i < mask_file->logical_x_dimension; i++ ) {
+                dx = i - x_sphere_center;
+                dy = j - y_sphere_center;
+                dz = k - z_spehere_center;
+                d  = sqrtf(dx * dx + dy * dy + dz * dz);
+                if ( d < radius ) {
+                    mask_file->real_values[pixel_counter] = 0.0;
+                }
+                else {
+                    mask_file->real_values[pixel_counter] = 1.0;
+                }
+                pixel_counter++;
+            }
+            pixel_counter += mask_file->padding_jump_value;
+        }
+    }
 }
