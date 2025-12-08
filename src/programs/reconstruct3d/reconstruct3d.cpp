87fa5c98022ea807aea0dda320c3b101a3972b47
Heidy Elkhaligy <heidy.elkhaligy@wisc.edu>
2025-12-08 11:32:03 -0500

    normalize_particles = false to work on RASTR

--- src/programs/reconstruct3d/reconstruct3d.cpp
+++ src/programs/reconstruct3d/reconstruct3d.cpp
@@ -390,8 +390,8 @@ bool Reconstruct3DApp::DoCalculation( ) {
 
     //	beam_tilt_x /= 1000.0f;
     //	beam_tilt_y /= 1000.0f;
-
-    original_box_size = input_stack.ReturnXSize( );
+    normalize_particles = false;
+    original_box_size   = input_stack.ReturnXSize( );
     // If resolution limit higher that Nyquist, do not do binning
     if ( resolution_limit_rec < 2.0 * pixel_size )
         resolution_limit_rec = 0.0;
